6dd78181655c561783f59eb10127ac5d6cf78a88
BunnyBear1419 <aubreya100114@gmail.com>
2026-09-21 13:48:40 -0400

    Fix server workflow syntax error

--- ALU_Gauntlet/web/server.py
+++ ALU_Gauntlet/web/server.py
@@ -201,7 +201,10 @@ class WebControlCenter:
 <script src="https://translate.google.com/translate_a/element.js?cb=rslGoogleTranslateInit"></script>
 '''
 
-        if "</body>" in body:\n            body = body.replace("</body>", language_markup + "</body>", 1)\n\n        if "</nav></header>" in body:
+        if "</body>" in body:
+            body = body.replace("</body>", language_markup + "</body>", 1)
+
+        if "</nav></header>" in body:
             body = body.replace("</nav></header>", "</nav>" + profile_markup + "</header>", 1)
         elif "</header>" in body:
             body = body.replace("</header>", profile_markup + "</header>", 1)
