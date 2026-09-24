6fd27b19e42b52eee8cde24a737c11501e0100e6
BunnyBear1419 <aubreya100114@gmail.com>
2026-09-24 08:47:04 -0400

    Bump global shell stylesheet cache version

--- ALU_Gauntlet/web/server.py
+++ ALU_Gauntlet/web/server.py
@@ -698,7 +698,7 @@ window.rslGoogleTranslateInit=function(){
         if filename.endswith(".html"):
             app_css_tag = re.compile(r'<link\b[^>]*href=["\']/static/app\.css(?:\?[^"\']*)?["\'][^>]*>', re.I)
             if app_css_tag.search(body):
-                body = app_css_tag.sub('<link rel="stylesheet" href="/static/app.css?v=20260924-shell2">', body, count=1)
+                body = app_css_tag.sub('<link rel="stylesheet" href="/static/app.css?v=20260924-shell3">', body, count=1)
             elif re.search(r"</head>", body, flags=re.I):
                 body = body.replace("</head>", '<link rel="stylesheet" href="/static/app.css?v=20260924-shell2"></head>', 1)
 
