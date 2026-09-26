0e2088eea88ddb65c4ed8517ac9f7b988f161cfe
BunnyBear1419 <aubreya100114@gmail.com>
2026-09-26 10:07:08 -0400

    Test RSL daily ticket economy

--- tests/test_gauntlet_mechanics.py
+++ tests/test_gauntlet_mechanics.py
@@ -4,12 +4,30 @@ from ALU_Gauntlet.core.gauntlet_progression import (
     refresh_cost,
     match_points,
     season_reward_for_rank,
+    FREE_DAILY_TICKETS,
+    MAX_PURCHASED_TICKETS,
+    MAX_DAILY_TICKETS,
+    extra_ticket_cost,
+    daily_ticket_state,
 )
 
 
 def test_rsl_refresh_costs_match_alu_style_escalation():
     assert [refresh_cost(i) for i in range(5)] == [25_000, 75_000, 125_000, 175_000, 200_000]
     assert refresh_cost(20) == 200_000
+
+
+def test_daily_ticket_economy_does_not_carry_over_and_escalates_costs():
+    assert FREE_DAILY_TICKETS == 5
+    assert MAX_PURCHASED_TICKETS == 5
+    assert MAX_DAILY_TICKETS == 10
+    assert [extra_ticket_cost(i) for i in range(5)] == [10_000, 20_000, 35_000, 55_000, 80_000]
+    assert extra_ticket_cost(5) == 0
+    state = daily_ticket_state(3, 7)
+    assert state["free_tickets"] == 5
+    assert state["purchased_tickets"] == 3
+    assert state["remaining_tickets"] == 7
+    assert state["next_purchase_cost"] == 55_000
 
 
 def test_rsl_match_points_reward_margin_and_win():
